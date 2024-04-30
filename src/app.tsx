230c11bc1175c17daa84ebf7fbed167f505dc165
Jérémy Chatard <jchatard@breek.fr>
2024-04-30 10:06:36 +0200

    Update patch for preloading

--- src/app.tsx
+++ src/app.tsx
@@ -15,12 +15,14 @@ export default function App() {
   };
 
   const rewrite = (url: string) => {
+    console.groupCollapsed("rewrite");
     console.log("url", url);
-    console.log("rewrite", rewriteMap);
+    //console.log("rewrite", rewriteMap);
     // Of course can be what ever you want...
     const u = new URL(url, "http://localhost/");
     console.log("u.pathname", u.pathname)
 
+    console.groupEnd();
     const dest = rewriteMap[u.pathname] + u.search
 
     return rewriteMap[u.pathname] ? dest : url;
